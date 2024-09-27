983db52ff4579dd8010aaa64c3d051f562bc23a9
nikolay.dimitrov <nikolay.dimitrov@delasport.com>
2024-09-27 18:37:47 +0300

    Created step a step definition class and Test runner class (with @CucumberOptions set)

---
+++ src/test/java/stepDefinitions/GreenCartStepDefinition.java
@@ -0,0 +1,24 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GreenCartStepDefinition {
+
+    @Given("user is on GreenCard Landing page")
+    public void user_is_on_green_card_landing_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user searched with shortname {string} and excract actual name of product")
+    public void user_searched_with_shortname_and_excract_actual_name_of_product(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user search for same shortname in offers page to check if product exist")
+    public void user_search_for_same_shortname_in_offers_page_to_check_if_product_exist() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
