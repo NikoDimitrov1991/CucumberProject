4d7acf3a64be9bca75b09eec07604c0f85a38c27
nikolay.dimitrov <nikolay.dimitrov@delasport.com>
2024-09-30 15:50:45 +0300

    Applying Single Responsibility Principle (SRL) to refactor method user_search_for_same_shortname_in_offers_page and create new method switchToOfferPage

--- src/test/java/stepDefinitions/OfferPageStepDefinition.java
+++ src/test/java/stepDefinitions/OfferPageStepDefinition.java
@@ -18,15 +18,21 @@ public class OfferPageStepDefinition {
 
     @Then("user search for {string} shortname in offers page")
     public void user_search_for_same_shortname_in_offers_page(String shortName) throws InterruptedException {
-        testContextSetup.driver.findElement(By.linkText("Top Deals")).click();
-        Set<String> s1 = testContextSetup.driver.getWindowHandles();
-        Iterator<String> iterator = s1.iterator();
-        String parentWindow = iterator.next();
-        String childWindow = iterator.next();
-        testContextSetup.driver.switchTo().window(childWindow);
+        switchToOfferPage();
         testContextSetup.driver.findElement(By.xpath("//input[@type='search']")).sendKeys(shortName);
         Thread.sleep(2000);
         offerPageProductName = testContextSetup.driver.findElement(By.cssSelector("tr td:nth-child(1)")).getText();
+    }
+
+    public void switchToOfferPage() {
+        if (!testContextSetup.driver.getCurrentUrl().equalsIgnoreCase("https://rahulshettyacademy.com/seleniumPractise/#/offers")) {
+            testContextSetup.driver.findElement(By.linkText("Top Deals")).click();
+            Set<String> s1 = testContextSetup.driver.getWindowHandles();
+            Iterator<String> iterator = s1.iterator();
+            String parentWindow = iterator.next();
+            String childWindow = iterator.next();
+            testContextSetup.driver.switchTo().window(childWindow);
+        }
     }
 
     @Then("validate product name in offers page matches with Landing Page")
