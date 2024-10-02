b6cf1e14987204c51a09435d5b37dedda9455778
nikolay.dimitrov <nikolay.dimitrov@delasport.com>
2024-10-02 13:45:42 +0300

    Adding Implicit Wait to TestBase

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Properties;
 
 public class TestBase {
@@ -24,14 +25,14 @@ public class TestBase {
                 options.addArguments("--disable-search-engine-choice-screen");
                 driver = new ChromeDriver(options);
                 driver.manage().window().maximize();
-                driver.get(url);
             } else if (prop.getProperty("browser").equalsIgnoreCase("firefox")) {
                 FirefoxOptions options = new FirefoxOptions();
                 options.addArguments("--disable-search-engine-choice-screen");
                 driver = new FirefoxDriver(options);
                 driver.manage().window().maximize();
-                driver.get(url);
             }
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+            driver.get(url);
         }
         return driver;
     }
