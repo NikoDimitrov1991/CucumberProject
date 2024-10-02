968c31c0f843bb24130c13ea6972d415d0b370fb
nikolay.dimitrov <nikolay.dimitrov@delasport.com>
2024-10-02 13:19:45 +0300

    Creating a new scenario with feature file, CheckoutPage class in pageObject package with locators and methods

---
+++ src/test/java/features/checkout.feature
@@ -0,0 +1,14 @@
+Feature: Search and Place an order for Products
+
+  Scenario Outline: Search Experience for product search in both Home Page and Offers Page
+
+    Given user is on GreenCard Landing page
+    When user searched with shortname <Name> and extract actual name of product
+    And Added "3" items of the selected product to cart
+    Then User proceeds to Checkout and validate the <Name> items in checkout page
+    And verify user has ability to enter promo code and place the order
+
+    Examples:
+      | Name |
+      | Tom  |
+      | Beet |
---
+++ src/test/java/pageObjects/CheckoutPage.java
@@ -0,0 +1,30 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CheckoutPage {
+    public WebDriver driver;
+    By cartBag = By.cssSelector("[alt='Cart']");
+    By checkOutButton = By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]");
+    By promoBtn = By.cssSelector(".promoBtn");
+    By placeOrder = By.xpath("//button[contains(text(),'Place Order')]");
+    public CheckoutPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void CheckoutItems() {
+        driver.findElement(cartBag).click();
+        driver.findElement(checkOutButton).click();
+    }
+
+    public Boolean verifyPromoButton() {
+        return driver.findElement(promoBtn).isDisplayed();
+    }
+
+    public Boolean verifyPlaceOrder() {
+        return driver.findElement(placeOrder).isDisplayed();
+    }
+
+
+}
