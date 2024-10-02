0fbab959ecc2f93937535c6ce88e3d4bb4980b92
nikolay.dimitrov <nikolay.dimitrov@delasport.com>
2024-10-02 14:04:50 +0300

    Adding *.html and *.json reports to the tests

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-@CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", monochrome = true, tags = "@PlaceOrder")
+@CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", monochrome = true, tags = "@PlaceOrder or @OffersPage", plugin = {"html:target/cucumberReport.html", "json:target/cucumber.json"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 
     @Override
