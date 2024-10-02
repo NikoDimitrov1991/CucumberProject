c24574a75b8c6ebaa46bd5e31112c303e0b28732
nikolay.dimitrov <nikolay.dimitrov@delasport.com>
2024-10-02 13:47:59 +0300

    Creating CheckoutPageStepDefinition class and adding method for Checkout Page in  PageObjectManager

---
+++ src/test/java/stepDefinitions/CheckoutPageStepDefinition.java
@@ -0,0 +1,35 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Then;
+import org.testng.Assert;
+import pageObjects.CheckoutPage;
+import pageObjects.LandingPage;
+import utils.TestContextSetup;
+
+public class CheckoutPageStepDefinition {
+
+
+    public CheckoutPage checkoutPage;
+    LandingPage landingPage;
+    TestContextSetup testContextSetup;
+
+    // Constructor injection
+    public CheckoutPageStepDefinition(TestContextSetup testContextSetup) {
+        this.testContextSetup = testContextSetup;
+        this.landingPage = testContextSetup.pageObjectManager.getLandingPage();
+    }
+
+    @Then("verify user has ability to enter promo code and place the order")
+    public void verify_user_has_ability_to_enter_promo_code_and_place_the_order() {
+        checkoutPage = testContextSetup.pageObjectManager.getCheckoutPage();
+        Assert.assertTrue(checkoutPage.verifyPromoButton());
+        Assert.assertTrue(checkoutPage.verifyPlaceOrder());
+    }
+
+    @Then("^User proceeds to Checkout and validate the (.+) items in checkout page$")
+    public void user_proceeds_to_checkout_and_validate_the_items_in_checkout_page(String name) {
+        checkoutPage.CheckoutItems();
+    }
+
+
+}
