68886e46b51435d03343f103e381ba59a1fab4b4
nikolay.dimitrov <nikolay.dimitrov@delasport.com>
2024-10-02 16:07:14 +0300

    Adding extent reports dependencies, creating extent.properties file and adding the reports to plugin in the Runner class

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -4,7 +4,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-@CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", monochrome = true, tags = "@PlaceOrder or @OffersPage", plugin = {"html:target/cucumberReport.html", "json:target/cucumber.json"})
+@CucumberOptions(features = "src/test/java/features",
+        glue = "stepDefinitions",
+        monochrome = true, tags = "@PlaceOrder or @OffersPage",
+        plugin = {"html:target/cucumberReport.html",
+                "json:target/cucumber.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 
     @Override
